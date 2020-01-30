ec2e1a6502311eb8b460c93da336de1bfae1123a
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-01-30 14:31:57 -0500

    finishing ex 3

--- drills.js
+++ drills.js
@@ -103,6 +103,20 @@ function sortDrill(array) {
 
 // 2. Understanding quicksort
 
+function bubbleSort(array) {
+  let swaps = 0;
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      swap(array, i, i + 1);
+      swaps++;
+    }
+  }
+
+  if (swaps > 0) {
+    return bubbleSort(array);
+  }
+  return array;
+};
 
 function swap(array, i, j) {
   const tmp = array[i];
@@ -110,53 +124,58 @@ function swap(array, i, j) {
   array[j] = tmp;
 };
 
-function bubbleSort(array) {
-  let swaps = 0;
-  for (let i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-          swap(array, i, i + 1);
-          swaps++;
-      }
-  }
 
-  if (swaps > 0) {
-      return bubbleSort(array);
-  }
-  return array;
-};
-
-let listOfNums=[14, 17, 13, 15, 19, 10, 3, 16, 9, 12]
+let listOfNums = [14, 17, 13, 15, 19, 10, 3, 16, 9, 12];
 
 function partition(array, start, end) {
   const pivot = array[end - 1];
   let j = start;
   for (let i = start; i < end - 1; i++) {
-      if (array[i] <= pivot) {
-          swap(array, i, j);
-          j++;
-      }
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
   }
-  swap(array, end-1, j);
-  console.log(array)
+  swap(array, end - 1, j);
   return j;
 };
 
 
 function quickSort(array, start = 0, end = array.length) {
   if (start >= end) {
-      return array;
+    return array;
   }
   const middle = partition(array, start, end);
   array = quickSort(array, start, middle);
   array = quickSort(array, middle + 1, end);
   return array;
 };
-console.log(quickSort(listOfNums))
+// console.log(quickSort(listOfNums));
 
 
 //2. The Pivot could have either been 14 or 17 because the partition function
 //    puts all the values greater than the pivot value to its right
+// Given: [14, 17, 13, 15, 19, 10, 3, 16, 9, 12] show the list after second partitioning. 
 //A) When using the last item on the list as a pivot
-        //[3,  9, 10, 12, 19, 14, 17, 16, 13, 15]
+//[3,  9, 10, 12, 19, 14, 17, 16, 13, 15]
 //B) When using the first item on the list as a pivot
-      //[  ]
+//[9, 10, 3, 12, 13, 14, 15, 16, 19, 17]
+
+// 3. Implementing quicksort
+
+function qSort(array, start = 0, end = array.length) {
+  if (start >= end) {
+    return array;
+  }
+  const middle = partition(array, start, end);
+  array = quickSort(array, start, middle);
+  array = quickSort(array, middle + 1, end);
+  return array;
+}
+
+
+let dataSet = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+
+// console.log(qSort(dataSet));
+
+// 4. Implementing merge sort
