07c9dd24871940bec903c4c242cafc75ad0eba28
Angel Alicea <ala31996@gmail.com>
2020-01-30 12:47:12 -0500

    added solution to 2a working on 2 b

--- drills.js
+++ drills.js
@@ -53,9 +53,10 @@ function sortDrill(array) {
   return merge(left, right, array);
 };
 
-let listOfNums = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
+// let listOfNums = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
 
-console.log(sortDrill(listOfNums));
+// console.log(sortDrill(listOfNums));
+
 
 // What is the resulting list that will be sorted after 4 recursive calls? Sequence below:
 // [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
@@ -102,3 +103,60 @@ console.log(sortDrill(listOfNums));
 
 // 2. Understanding quicksort
 
+
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+};
+
+function bubbleSort(array) {
+  let swaps = 0;
+  for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+          swap(array, i, i + 1);
+          swaps++;
+      }
+  }
+
+  if (swaps > 0) {
+      return bubbleSort(array);
+  }
+  return array;
+};
+
+let listOfNums=[14, 17, 13, 15, 19, 10, 3, 16, 9, 12]
+
+function partition(array, start, end) {
+  const pivot = array[end - 1];
+  let j = start;
+  for (let i = start; i < end - 1; i++) {
+      if (array[i] <= pivot) {
+          swap(array, i, j);
+          j++;
+      }
+  }
+  swap(array, end-1, j);
+  console.log(array)
+  return j;
+};
+
+
+function quickSort(array, start = 0, end = array.length) {
+  if (start >= end) {
+      return array;
+  }
+  const middle = partition(array, start, end);
+  array = quickSort(array, start, middle);
+  array = quickSort(array, middle + 1, end);
+  return array;
+};
+console.log(quickSort(listOfNums))
+
+
+//2. The Pivot could have either been 14 or 17 because the partition function
+//    puts all the values greater than the pivot value to its right
+//A) When using the last item on the list as a pivot
+        //[3,  9, 10, 12, 19, 14, 17, 16, 13, 15]
+//B) When using the first item on the list as a pivot
+      //[  ]
